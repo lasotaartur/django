79a05f537342d42829825708756351082daf90a4
lasotaartur <lasota.artur@gmail.com>
2019-06-28 16:35:37 +0200

    add polls routes and generic actions

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-from django.http import HttpResponse
+from polls.models import Question, Choice
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
